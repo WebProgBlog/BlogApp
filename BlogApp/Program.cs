9494e7920a0ce5fe9023ea6b336f37d062cb5549
iclal millik <icllmllk03@gmail.com>
2025-05-30 23:10:54 +0300

    DataLayer klasörü altında entityframework,code first yaklaşımıyla veritabanı bağlantısı ve migrations oluşturulması.

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,9 +1,17 @@
+using BlogApp.DataLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sql_connection")));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
