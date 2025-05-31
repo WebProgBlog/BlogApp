e674f88c159a86f7ee887df6aa8b553013f9c665
iclal millik <icllmllk03@gmail.com>
2025-05-31 23:01:32 +0300

    Businness katmanı oluşturuldu gerekli service ve manager sınıfları tanımlandı.

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7273820cb715276c9ee58ef81bca462ccf9fe3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
