68a2cc47251476b2299987d3358541f855a0439b
sevvalinal <sevvalinal16@gmail.com>
2025-06-05 19:52:56 +0300

    Blog sitesi için HTML ve CSS tasarımı eklendi Sayfa şablonları oluşturuldu (anasayfa, detay, giriş, kayıt) Responsive tasarım yapıldı Ortak alanlar için düzenli yapı kuruldu (layout ve parça görünümler) Temel stil ve görsel düzenlemeler yapıldı

---
+++ BlogApp/Models/ProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BlogApp.Models
+{
+    public class ProfileViewModel
+    {
+
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+
+        public string? Email { get; set; }
+        public string? Image { get; set; }
+
+
+
+
+    }
+}
